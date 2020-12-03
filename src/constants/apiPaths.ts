07d6f781fb334d8b617d8d86ed1bb7fd05365a33
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-12-04 00:07:41 +0100

    feat: add cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,8 @@ const API_PATHS = {
   product: 'https://mlmufxfj36.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://mlmufxfj36.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://xu0z0hbqm1.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://mlmufxfj36.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://mlmufxfj36.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://macbook-cart-api.eu-west-1.elasticbeanstalk.com/api'
 };
 
 export default API_PATHS;
