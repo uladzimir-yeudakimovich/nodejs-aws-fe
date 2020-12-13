d8594dca02c631ed9d55f065cffba7ca607c6e2b
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2020-12-13 01:52:18 +0100

    feat: add bff

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,7 @@
 const API_PATHS = {
-  product: 'https://mlmufxfj36.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://xu0z0hbqm1.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://xv6yeihkr4.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'http://uladzimiryeudakimovich-bff-api-dev.eu-west-1.elasticbeanstalk.com'
 };
 
 export default API_PATHS;
